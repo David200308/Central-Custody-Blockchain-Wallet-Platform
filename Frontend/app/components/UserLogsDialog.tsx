23fc2597044253bd3d344fee49d3b88422500591
David Jiang <jiangguanlin2003@outlook.com>
2025-01-15 23:15:56 +0800

    feat: update frontend dashboard

---
+++ Frontend/app/components/UserLogsDialog.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+
+interface UserLogsDialogProps {
+    open: boolean;
+    closeDialog: () => void;
+}
+
+interface Logs {
+    "log_id": number;
+    "user_id": number;
+    "log_time": string;
+    "content": string;
+}
+
+
+export function UserLogsDialog({ open, closeDialog }: UserLogsDialogProps) {
+    const [logs, setLogs] = useState<Logs[]>([]);
+
+    const fetchLogs = async () => {
+        const logsResponse = await fetch('/api/user/logs');
+        if (!logsResponse.ok) {
+            throw new Error("Failed to fetch logs");
+        }
+        const logsData = await logsResponse.json();
+        setLogs(logsData);
+    };
+
+    useEffect(() => {
+        if (open) {
+            fetchLogs();
+        }
+    }, []);
+
+    return (
+        <Dialog open={open} onClose={closeDialog} className="relative z-50 text-black">
+            <div className="fixed inset-0 bg-black bg-opacity-30" aria-hidden="true"></div>
+            <div className="fixed inset-0 flex items-center justify-center p-4">
+                <DialogPanel className="max-w-lg space-y-6 border bg-white p-12 rounded-md">
+                    <DialogTitle className="font-bold text-2xl">User Logs</DialogTitle>
+                    <div className="space-y-6">
+                        <ul className="mt-4 space-y-4">
+                            {logs && logs.map((log, index) => (
+                                <li key={index} className="p-4 border-l-4 border-gray-400 rounded-lg">
+                                    <span className="font-semibold">Log Time:</span> <span>{
+                                        log.log_time.split("T")[0] + " " + log.log_time.split("T")[1].split(".")[0]
+                                    }</span>
+                                    <br />
+                                    <span className="font-semibold">Content:</span> <span>{log.content}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </DialogPanel>
+            </div>
+        </Dialog>
+    );
+}
--- Frontend/app/routes/dashboard.tsx
+++ Frontend/app/routes/dashboard.tsx
@@ -1,8 +1,9 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 // eslint-disable-next-line import/no-unresolved
 import { useNavigate } from "@remix-run/react";
-import { useQuery } from "@tanstack/react-query";
-import { SetupPasskeyDialog } from "../components/SetupPasskeyDialog";
+// import { useQuery } from "@tanstack/react-query";
+import { QRCodeSVG } from "qrcode.react";
+import { UserLogsDialog } from "~/components/UserLogsDialog";
 
 interface User {
     id: string;
@@ -18,7 +19,7 @@ interface Logs {
 }
 
 async function verifyToken() {
-        const response = await fetch("/api/user/token", {
+    const response = await fetch("/api/user/token", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -41,19 +42,10 @@ export default function Dashboard() {
         email: "",
         passkeyEnabled: false,
     });
-    const [passkeyOpen, setPasskeyOpen] = useState(false);
-    const [logs, setLogs] = useState<Logs[]>([]);
+    const [walletAddress, setWalletAddress] = useState<string>("0x00000000");
+    const [userLogsOpen, setUserLogsOpen] = useState(false);
 
     const navigate = useNavigate();
-
-    const fetchLogs = async () => {
-        const logsResponse = await fetch('/api/user/logs');
-        if (!logsResponse.ok) {
-            throw new Error("Failed to fetch logs");
-        }
-        const logsData = await logsResponse.json();
-        setLogs(logsData);
-    };
 
     const logout = async () => {
         const response = await fetch("/api/user/logout", {
@@ -63,11 +55,11 @@ export default function Dashboard() {
             },
             body: JSON.stringify({}),
         });
-    
+
         if (!response.ok) {
             throw new Error("Failed to logout");
         }
-    
+
         return response.json();
     }
 
@@ -75,7 +67,6 @@ export default function Dashboard() {
         verifyToken().then((data) => {
             if (data.isValid) {
                 setUser(data.user);
-                fetchLogs();
             }
         }).catch((error) => {
             console.log("Failed to verify token:", error);
@@ -87,43 +78,57 @@ export default function Dashboard() {
         <div className="max-w-4xl mx-auto mt-10">
             <h1 className="text-4xl font-bold text-black">Dashboard</h1>
 
-            <div className="mt-8 p-6 bg-white shadow rounded-lg text-black">
-                <h2 className="text-2xl font-semibold">User Information</h2>
-                <div className="mt-4">
-                    <p className="text-lg mt-2">
-                        <span className="font-medium">Email:</span> {user.email}
-                    </p>
-                    <p className="text-lg mt-2">
-                        <span className="font-medium">Polygon Wallet Address :</span> 0x00000000
-                    </p>
-                    
-                    <div className="mt-4">
-                        <button
-                            onClick={() => logout().then(() => {
-                                alert("Logged out successfully");
-                                navigate("/login");
-                            })}
-                            className="mt-4 px-6 py-2 w-[40%] bg-black text-white rounded hover:bg-gray-800"
-                        >
-                            Logout
-                        </button>
+            <div className="mt-8 p-6 bg-white shadow rounded-lg text-black flex">
+                <div className="flex-1">
+                    <h2 className="text-2xl font-semibold">User Information</h2>
+                    <div className="mt-4 space-y-4">
+                        <div>
+                            <p className="text-lg mt-2">
+                                <span className="font-medium">Email:</span> {user.email}
+                            </p>
+                            <p className="text-lg mt-2">
+                                <span className="font-medium">Polygon Wallet Address:</span> 0x00000000
+                            </p>
+                        </div>
+                        <div>
+                            <button
+                                className="mt-2 px-6 py-2 w-[60%] bg-black text-white rounded hover:bg-gray-800"
+                            >
+                                Sign Message / Transaction
+                            </button>
+                        </div>
+                        <div>
+                            <button
+                                className="mt-2 px-6 py-2 w-[60%] bg-black text-white rounded hover:bg-gray-800"
+                            >
+                                Signature Requests History
+                            </button>
+                        </div>
+                        <div>
+                            <button
+                                onClick={() => setUserLogsOpen(true)}
+                                className="mt-2 px-6 py-2 w-[60%] bg-black text-white rounded hover:bg-gray-800"
+                            >
+                                User Logs
+                            </button>
+                            <UserLogsDialog open={userLogsOpen} closeDialog={() => setUserLogsOpen(false)} />
+                        </div>
+                        <div className="mt-4">
+                            <button
+                                onClick={() => logout().then(() => {
+                                    alert("Logged out successfully");
+                                    navigate("/login");
+                                })}
+                                className="mt-2 px-6 py-2 w-[60%] bg-black text-white rounded hover:bg-gray-800"
+                            >
+                                Logout
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="mt-8 p-6 shadow-lg rounded-lg text-black">
-                <h2 className="text-3xl font-bold mb-4">User Logs</h2>
-                <ul className="mt-4 space-y-4">
-                    {logs && logs.map((log, index) => (
-                        <li key={index} className="p-4 border-l-4 border-gray-400 rounded-lg">
-                            <span className="font-semibold">Log Time:</span> <span>{
-                                log.log_time.split("T")[0] + " " + log.log_time.split("T")[1].split(".")[0]
-                            }</span>
-                            <br />
-                            <span className="font-semibold">Content:</span> <span>{log.content}</span>
-                        </li>
-                    ))}
-                </ul>
+                <div className="mt-4 mr-20 flex justify-center items-center">
+                    <QRCodeSVG value={walletAddress} size={200} />
+                </div>
             </div>
         </div>
     );
