755d468c36db4f57effc96e85a2a208b5569df77
David Jiang <jiangguanlin2003@outlook.com>
2025-01-22 09:47:38 +0800

    feat: add get wallet controller

--- Backend/src/controllers/wallet.ts
+++ Backend/src/controllers/wallet.ts
@@ -1,18 +1,67 @@
 import { Body, Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { WalletServices } from '../services/wallet';
 import { Response, Request } from 'express';
+import { UserServices } from '../services/user';
+import { JwtPayload } from 'jsonwebtoken';
+import { verifyToken } from '../utils/auth';
 
 @Controller("wallet")
 export class WalletController {
-    constructor(private readonly walletService: WalletServices) { }
+    constructor(
+        private readonly walletService: WalletServices,
+        private readonly userService: UserServices
+    ) { }
+
+    @Get("")
+    async getWalletAddress(@Req() request: Request, @Res({ passthrough: true }) response: Response) {
+        const token = request.cookies?.token;
+        if (!token) {
+            response.status(HttpStatus.UNAUTHORIZED).json({
+                message: 'Unauthorized',
+            });
+            return;
+        }
+
+        const payload: JwtPayload = await verifyToken(token).catch((err) => {
+            console.error('Token verification failed:', err);
+            throw new Error('Unauthorized');
+        });
+
+        if (typeof payload !== "object" || !(typeof payload.aud === 'string')) {
+            response.status(HttpStatus.UNAUTHORIZED).json({
+                message: 'Unauthorized'
+            });
+            return;
+        }
+
+        const data = await this.userService.getUserById(parseInt(payload.aud, 10));
+        if (!data) {
+            response.status(HttpStatus.NOT_FOUND).json({
+                message: 'User not found',
+            });
+            return;
+        }
+        try { 
+            const wallet = await this.walletService.getWalletByUserId(data.id);
+            response.status(HttpStatus.OK).json({
+                success: true,
+                walletAddress: wallet,
+            });
+        } catch (error) {
+            response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                message: error.message || 'Failed to fetch gas fee',
+            });
+        }
+    }
 
     @Post("create")
-    async createWallet(@Req() req: Request, @Res() res: Response) {}
+    async createWallet(@Req() req: Request, @Res() res: Response) { }
 
     @Post("sign")
-    async createSign(@Req() req: Request, @Res() res: Response) {}
+    async createSign(@Req() req: Request, @Res() res: Response) { }
 
     @Post("verify")
-    async VerifySign(@Req() req: Request, @Res() res: Response) {}
+    async VerifySign(@Req() req: Request, @Res() res: Response) { }
 
 }
--- Backend/src/services/wallet.ts
+++ Backend/src/services/wallet.ts
@@ -1,7 +1,15 @@
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
 import { pool } from "../database/database";
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class WalletServices {
+    private readonly logger = new Logger(WalletServices.name);
+    
+    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) { }
+
+    async getWalletByUserId(userId: number): Promise<String> {
+        return "0x1234567890";
+    };
 
 }
