faaf900865dcbe834871821fd11603c7f4037863
David Jiang <jiangguanlin2003@outlook.com>
2025-02-22 15:48:56 +0800

    fix: wallet sql

--- Backend/src/database/sql/wallet/wallet.ts
+++ Backend/src/database/sql/wallet/wallet.ts
@@ -1,4 +1,4 @@
-export const ADD_WALLET_SQL = 'INSERT INTO auth (wallet_uuid, user_id, wallet_address) VALUES ($1, $2, $3) RETURNING wallet_id';
+export const ADD_WALLET_SQL = 'INSERT INTO wallet (wallet_uuid, user_id, wallet_address) VALUES ($1, $2, $3) RETURNING wallet_id';
 
 export const GET_WALLET_BY_UUID_SQL = 'SELECT * FROM wallet WHERE wallet_uuid = $1';
 
