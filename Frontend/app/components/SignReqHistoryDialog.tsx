9bbb141c31aed68eac92f93ee233865934972c29
David Jiang <jiangguanlin2003@outlook.com>
2025-02-22 21:33:27 +0800

    fix: frontend display issue

--- Frontend/app/components/SignReqHistoryDialog.tsx
+++ Frontend/app/components/SignReqHistoryDialog.tsx
@@ -39,7 +39,7 @@ export function SignReqHistoryDialog({ open, closeDialog }: SignReqHistoryDialog
     }, []);
 
     const totalPages = Math.ceil(signReq.length / signreqPerPage);
-    const paginatedSignReq = signReq.slice((currentPage - 1) * signreqPerPage, currentPage * signreqPerPage);
+    const paginatedSignReq = signReq.slice((currentPage - 1) * signreqPerPage, currentPage * signreqPerPage) ?? [];
 
     const handleNextPage = () => {
         if (currentPage < totalPages) {
@@ -62,7 +62,7 @@ export function SignReqHistoryDialog({ open, closeDialog }: SignReqHistoryDialog
                     <div className="space-y-6">
                         <ul className="mt-4 space-y-4">
                             {
-                                signReq ?
+                                signReq && paginatedSignReq ?
                                 paginatedSignReq.map((signReq) => (
                                     <li key={signReq.signrequest_id} className="p-4 border-l-4 border-gray-400 rounded-lg">
                                         <span className="font-semibold">Request Time:</span>{" "}
